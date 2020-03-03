dc9eb244d2e72629d37ff4472401c5722d332c57
wsingleton <wezley.singleton@gmail.com>
2020-03-03 00:07:53 -0500

    init repo and project

---
+++ src/test/java/com/revature/rms/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rms.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
