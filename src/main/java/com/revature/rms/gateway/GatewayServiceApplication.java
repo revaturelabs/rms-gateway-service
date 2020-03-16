f475e7269eccff073dca63e2234f1a4d87cd3e71
Wezley <wezley.singleton@gmail.com>
2020-03-15 23:53:07 -0400

    added employee route config

--- src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
+++ src/main/java/com/revature/rms/gateway/GatewayServiceApplication.java
@@ -1,15 +1,30 @@
 package com.revature.rms.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/employees/**")
+						.uri("lb://employee-service")
+						.id("employee-service"))
+				.build();
 	}
 
 }
